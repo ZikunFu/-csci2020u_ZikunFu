bf47b56c15e66efb9e9327991080234f8062f5e4
Voice <git497075391>
2021-03-06 10:44:58 -0500

    lab06 finished

---
+++ lab06/src/sample/Main.java
@@ -0,0 +1,44 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.paint.Color;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+    private static double[] avgHousingPricesByYear = {
+            247381.0,264171.4,287715.3,294736.1,
+            308431.4,322635.9,340253.0,363153.7
+    };
+    private static double[] avgCommercialPricesByYear = {
+            1121585.3,1219479.5,1246354.2,1295364.8,
+            1335932.6,1472362.0,1583521.9,1613246.3
+    };
+    private static String[] ageGroups = {
+            "18-25", "26-35", "36-45", "46-55", "56-65", "65+"
+    };
+    private static int[] purchasesByAgeGroup = {
+            648, 1021, 2453, 3173, 1868, 2247
+    };
+    private static Color[] pieColours = {
+            Color.AQUA, Color.GOLD, Color.DARKORANGE,
+            Color.DARKSALMON, Color.LAWNGREEN, Color.PLUM
+    };
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("lab06");
+        primaryStage.setScene(new Scene(root, 600, 350));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
